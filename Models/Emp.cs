d0ffeb9a92df1f6812d42ddbaf1ccd1b9479ed8e
rishabh817 <rishabhagarwal817@gmail.com>
2020-12-03 16:02:49 +0530

    Second commit

---
+++ Models/Emp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppBranching.Models
+{
+    public class Emp
+    {
+        public int Eid { get; set; }
+        public string EName { get; set; }
+        public string EDesig { get; set; }
+
+        public DateTime EDOJ { get; set; }
+    }
+}
