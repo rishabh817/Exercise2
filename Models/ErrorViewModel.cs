9e892767b2834a3ec02ccfe531c9ae8d1063569b
rishabh817 <rishabhagarwal817@gmail.com>
2020-12-03 16:01:04 +0530

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppBranching.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
